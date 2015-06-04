ce4df65c27ba06e7b33950fdfbb70bd1969263ef
MichaelAPace <mapace@gmail.com>
2015-06-04 15:28:20 -0600

    commented out some parts to clean it up some

--- grader.js
+++ grader.js
@@ -1,15 +1,15 @@
 module.exports = {
 
-Grader
-
-Write a function letterGrader which takes a test score and returns the equivalent letter grade.
-
-A: 90-100
-B: 80-99
-C: 70-79
-D: 60-69
-F: 0-59
-letterGrader(98);
+// Grader
+//
+// Write a function letterGrader which takes a test score and returns the equivalent letter grade.
+//
+// A: 90-100
+// B: 80-99
+// C: 70-79
+// D: 60-69
+// F: 0-59
+// letterGrader(98);
 //=> 'A'
 
 
@@ -30,12 +30,12 @@ letterGrader: function(a){
 
 
 
-Average
-
-Write a function 'averageScore` which takes an array of test scores and returns the average score.
-
-averageScore([90, 95, 87, 60]);
-//=> 83
+// Average
+//
+// Write a function 'averageScore` which takes an array of test scores and returns the average score.
+//
+// averageScore([90, 95, 87, 60]);
+// //=> 83
 
 averageScore: function(arr){
 var sum = 0;
@@ -49,13 +49,13 @@ return average;
 
 
 
-
-Median
-
-Write a function medianScore which takes an array of test scores and calculates the median score. The median is the middle value. If there are an even number of scores, calculate the average of the middle two scores.
-
-medianScore([52,80,80,86,94])
-=> 80
+//
+// Median
+//
+// Write a function medianScore which takes an array of test scores and calculates the median score. The median is the middle value. If there are an even number of scores, calculate the average of the middle two scores.
+//
+// medianScore([52,80,80,86,94])
+// => 80
 
 medianScore: function(values) {
 
@@ -73,12 +73,12 @@ medianScore: function(values) {
 
 
 
-MODE
-
-Write a function modeScore which takes an array of test scores and calculates the mode score. The mode is the value that appears most often.
-
-modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])
-//=> 92
+// MODE
+//
+// Write a function modeScore which takes an array of test scores and calculates the mode score. The mode is the value that appears most often.
+//
+// modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])
+// //=> 92
 
 
 modeScore: function(values) {
